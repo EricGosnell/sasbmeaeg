89ae911758bf79d6041319c3ef77c7e251438c2a
Eric Gosnell <eric.gosnell@colorado.edu>
2026-07-04 14:10:07 -0600

    Update main to test ipv4 host in github deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>SASBMEAEG</h1>
+<p>Welcome to Sequences and Series by Max Eaton and Eric Gosnell</p>
