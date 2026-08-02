26ce30bd5a837b6525192032d723d0d225b6f78a
EricGosnell <eric.gosnell@colorado.edu>
2026-08-02 14:06:00 -0600

    Update rank rule to map rank to number

--- yard/frontend/src/lib/ruleWorker.ts
+++ yard/frontend/src/lib/ruleWorker.ts
@@ -19,7 +19,24 @@ function runRule(
 		}
 
 		function rank(card) {
-			return card.rank;
+			const rankMap = {
+			   A: 1,
+			   "2": 2,
+			   "3": 3,
+			   "4": 4,
+			   "5": 5,
+			   "6": 6,
+			   "7": 7,
+			   "8": 8,
+			   "9": 9,
+			   "10": 10,
+			   J: 11,
+			   Q: 12,
+			   K: 13,
+			   Joker1: 0,
+			   Joker2: 0
+			};
+			return rankMap[card.rank];
 		}
 		
 		function color(card) {
